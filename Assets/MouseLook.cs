d58f0768ce8467062ea16288d31d5ba3081da774
Bar Ronen <barzie02@gmail.com>
2020-11-30 18:58:51 +0200

    Created Character with Movement+MouseLook script

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public Transform playerBody;
+    public float _mouseSensitivity = 100f;
+
+    private float _xRotation = 0f;
+
+
+
+    void Start()
+    {
+        //Reset Cursor position
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void Update()
+    {
+        //Mouse look inputs
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity * Time.deltaTime;
+        _xRotation -= mouseY;
+
+        //Clamp mouse look rotation
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController _controller;
+
+    [SerializeField]
+    private float _jumpSpeed = 2f;
+    [SerializeField]
+    private float _gravity = 9.81f;
+    [SerializeField]
+    private float _moveSpeed = 4f;
+  
+    private float _directionY;
+ 
+    void Start()
+    {
+        //Assign _controller to CharacterController component
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        //CharacterController Inputs
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //Movement Vector
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+
+        //Jumping feature
+        if (_controller.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                _directionY = _jumpSpeed;
+            }
+        }
+
+         direction.y = _directionY;
+        _directionY -= _gravity * Time.deltaTime;
+        _controller.Move(direction * _moveSpeed * Time.deltaTime);
+    }
+}
