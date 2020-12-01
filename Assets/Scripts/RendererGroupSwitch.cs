5b79ca24c93ce29b815c506196cf91ea17deabb8
Amir Aharoni <Aharoniamirr@gmail.com>
2020-12-01 18:38:36 +0200

    Island Basic colliders
    Needs some major updates but the base is fine for now

---
+++ Assets/Scripts/RendererGroupSwitch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[ExecuteInEditMode]
+public class RendererGroupSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    List<MeshRenderer> renderers = new List<MeshRenderer>();
+    public bool Toggle;
+    private bool tempToggle;
+    private void Update()
+    {
+        if(tempToggle == Toggle) { return; }
+        else
+        {
+            Refresh();
+            tempToggle = Toggle;
+        }
+
+    }
+    public void Refresh()
+    {
+        renderers.Clear();
+        renderers.AddRange(GetComponentsInChildren<MeshRenderer>());
+        foreach (MeshRenderer item in renderers)
+        {
+            item.enabled = Toggle;
+        }
+
+    }
+}
