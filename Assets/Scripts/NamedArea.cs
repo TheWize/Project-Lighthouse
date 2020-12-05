acdb489ed0052cf74798043db2c03fe9ece56a99
Bar Ronen <barzie02@gmail.com>
2020-12-05 20:06:50 +0200

    Terrain Update

--- Assets/Scripts/NamedArea.cs
+++ Assets/Scripts/NamedArea.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class NamedArea : MonoBehaviour
 {
+    [SerializeField] private string setText;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            TextDisplay.Instance.setText("Welcome to palm springs");
+            TextDisplay.Instance.setText(setText);
             TextDisplay.Instance.ShowText();
         }
     }
