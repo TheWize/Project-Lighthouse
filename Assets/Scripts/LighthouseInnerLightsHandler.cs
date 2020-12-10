8faea2bc8848f054faef2c925b250e0b024cd336
Amir Aharoni <Aharoniamirr@gmail.com>
2020-12-10 14:42:12 +0200

    Added Light animation

---
+++ Assets/Scripts/LighthouseInnerLightsHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LighthouseInnerLightsHandler : MonoBehaviour
+{
+    private bool canActivate;
+
+    private void Start()
+    {
+        canActivate = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!canActivate)
+        {
+            return;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponentInParent<Animator>().SetTrigger("Trigger");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            canActivate = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            canActivate = false;
+        }
+    }
+}
