cf1242ccdf5f02afeb4e45f7c588d9cc6aefe11f
Bar Ronen <barzie02@gmail.com>
2020-12-14 12:08:10 +0200

    Final 1.1 - Terrain update + sounds added to library

---
+++ Assets/Scripts/Win.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+    private bool canTrigger;
+    private bool mouseDown { get => Input.GetMouseButtonDown(0); }
+    // Start is called before the first frame update
+    void Start()
+    {
+        canTrigger = false;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+        }
+    }
+}
